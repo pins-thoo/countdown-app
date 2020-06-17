660cd0d679154176c4267ea047fae444d7cca766
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-17 16:15:19 +0800

    added components to clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+// import component feature in react
+import './App.css';
 
+// ES6 CLASS
 class App extends Component {
   render() {
     return (
-      <div> Countdown Champ, App Component</div>
+      <div className="App">
+        <div className="App-title">Countdown to December 25, 2020</div>
+        <div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-mins">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+           <input placeholder='new date' />
+           <button>Submit</button>
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App></App>,
+  <App />, // instead of <App></App> equivalent of <App />
   document.getElementById('root')
 );
